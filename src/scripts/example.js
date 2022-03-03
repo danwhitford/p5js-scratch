59f9f74f9ad13fec69f4536253984000954a5dc7
Daniel Whitford <whitford.dsw@gmail.com>
2022-03-03 14:56:40 +0000

    move around
    mend

---
+++ src/scripts/example.js
@@ -0,0 +1,9 @@
+function setup(){
+    createCanvas(500, 500);
+    background(64);
+}
+
+function draw(){
+    fill(255);
+    ellipse(mouseX, mouseY, 20, 20);
+}
---
+++ src/scripts/squares.js
@@ -0,0 +1,39 @@
+let c1;
+let c2;
+let numSquares;
+let squareSize;
+let counter = 0;
+
+function setup() {
+    createCanvas(501, 501);
+    // background(64);
+    // noLoop();
+    // frameRate(1)
+}
+
+function draw() {
+    for (let n = 0; n < 2; ++n) {
+        c1 = [random(255), random(255), random(255), random(55, 200),]
+        c2 = [random(255), random(255), random(255), random(55, 200),]
+        numSquares = n === 0 ? Math.floor(Math.random() * 32) : Math.floor(Math.random() * 8)
+        squareSize = 500 / numSquares;
+
+        for (let y = 0; y < numSquares; ++y) {
+            for (let x = 0; x < numSquares; ++x) {
+                const c = y % 2 === 0
+                    ? x % 2 === 0 ? c1 : c2
+                    : x % 2 === 0 ? c2 : c1
+
+                fill(...c);
+
+                rect(x * squareSize, y * squareSize, squareSize, squareSize);
+            }
+        }
+    }
+    if (counter > 10) {
+        noLoop()
+    }
+    saveCanvas('canvas'+counter, 'png')
+    counter++
+}
+
